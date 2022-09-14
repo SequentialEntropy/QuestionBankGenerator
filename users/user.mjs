bcd0672de6223c4bba52b0fdb23eafcc36acf69c
Skytec711 <asahi79289@gmail.com>
2022-09-14 16:24:04 +0800

    Feat: Logger messages for user.mjs

--- users/user.mjs
+++ users/user.mjs
@@ -23,15 +23,21 @@ class User {
         this._filePath = filePath;
         this._load(data);
         this._logEnabled = logEnabled;
+
+        this._log("Initialised");
     }
     static async init(options, logEnabled=false) {
+        log("User static", "Initialising User");
         let filePath;
         let data = defaults;
         if (options.hasOwnProperty("id")) { // Load existing user by id
+            log("User static", `ID ${options.id} passed when initialising`);
             filePath = join(__dirname, "userFiles", `user${options.id.toString()}.json`);
             data = await User._readFromFile(filePath);
         } else { // Create new user by fields and auto id
+            log("User static", "ID not passed when initialising");
             options.id = counter.create();
+            log("User static", `New ID ${options.id} generated`);
             filePath = join(__dirname, "userFiles", `user${options.id.toString()}.json`);
             data.id = options.id;
             data.username = options.username;
@@ -44,23 +50,39 @@ class User {
         return new User(data, filePath, logEnabled);
     }
     static async _readFromFile(filePath) {
+        log("User static", "Read function called", filePath);
+
         const rawData = await fs.readFile(filePath);
+        log("User static", "File read", rawData);
         const data = JSON.parse(rawData);
+        log("User static", "String -> JSON - Parsed read file", data);
+
         return data;
     }
     async _load(data) {
+        this._log("Load function called");
+        
         this._id = data.id;
         this._username = data.username;
         this._email = data.email;
         this._password = data.password;
         this._questions = data.questions;
         this._sets = data.sets;
+
+        this._log("Load done");
     }
     static async _writeToFile(data, filePath) {
+        log("User static", "Write function called", filePath);
+
         const rawData = JSON.stringify(await data, null, 4);
+        log("User static", "JSON -> String - Prepared stringified data for writing");
+        fs.writeFile(filePath, rawData);
+        log("User static", "File written", filePath);
+
         return fs.writeFile(filePath, rawData);
     }
     async _save() {
+        this._log("Save function called");
         return {
             id: this._id,
             username: this._username,
