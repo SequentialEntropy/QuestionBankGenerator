8bf3a43dd7fc7cbe1928187d9e49dc7282c37172
Skytec711 <asahi79289@gmail.com>
2022-09-14 16:34:06 +0800

    Feat: Logger messages for classroom.mjs

--- classrooms/classroom.mjs
+++ classrooms/classroom.mjs
@@ -24,15 +24,21 @@ class Classroom {
         this._filePath = filePath;
         this._load(data);
         this._logEnabled = logEnabled;
+
+        this._log("Initialised");
     }
     static async init(options, logEnabled=false) {
+        log("Classroom static", "Initialising Classroom");
         let filePath;
         let data = defaults;
         if (options.hasOwnProperty("id")) {
+            log("Classroom static", `ID ${options.id} passed when initialising`);
             filePath = join(__dirname, "classroomFiles", `classroom${options.id.toString()}.json`);
             data = await Classroom._readFromFile(filePath);
         } else {
+            log("Classroom static", "ID not passed when initialising");
             options.id = counter.create();
+            log("Classroom static", `New ID ${options.id} generated`);
             filePath = join(__dirname, "classroomFiles", `classroom${options.id.toString()}.json`);
             data.id = options.id;
             data.name = options.name;
@@ -45,36 +51,19 @@ class Classroom {
         }
         return new Classroom(data, filePath, logEnabled);
     }
-    readFromFile(filePath) {
-        let classroomRawData = fs.readFileSync(filePath);
-        let classroomParsed = JSON.parse(classroomRawData);
-        this._id = classroomParsed.id || defaults.id;
-        this._name = classroomParsed.name || defaults.name;
-        this._description = classroomParsed.description || defaults.description;
-        this._sets = classroomParsed.sets || defaults.sets;
-        this._owner = classroomParsed.owner || defaults.owner;
-        this._teachers = classroomParsed.teachers || defaults.teachers;
-        this._students = classroomParsed.students || defaults.students;
-    }
-    writeToFile(filePath) {
-        let classroomData = {
-            id: this._id,
-            name: this._name,
-            description: this._description,
-            sets: this._sets,
-            owner: this._owner,
-            teachers: this._teachers,
-            students: this._students
-        };
-        let classroomRawData = JSON.stringify(classroomData, null, 4);
-        fs.writeFileSync(filePath, classroomRawData);
-    }
     static async _readFromFile(filePath) {
+        log("Classroom static", "Read function called", filePath);
+
         const rawData = await fs.readFile(filePath);
+        log("Classroom static", "File read", rawData);
         const data = JSON.parse(rawData);
+        log("Classroom static", "String -> JSON - Parsed read file", data);
+
         return data;
     }
     async _load(data) {
+        this._log("Load function called");
+
         this._id = data.id;
         this._name = data.name;
         this._description = data.description;
@@ -82,12 +71,19 @@ class Classroom {
         this._owner = data.owner;
         this._teachers = data.teachers;
         this._students = data.students;
+
+        this._log("Load done");
     }
     static async _writeToFile(data, filePath) {
+        log("Classroom static", "Write function called", filePath);
+
         const rawData = JSON.stringify(await data, null, 4);
-        return fs.writeFile(filePath, rawData);
+        log("Classroom static", "JSON -> String - Prepared stringified data for writing");
+        fs.writeFile(filePath, rawData);
+        log("Classroom static", "File written", filePath);
     }
     async _save() {
+        this._log("Save function called");
         return {
             id: this._id,
             name: this._name,
