39610f24a1cdc1e9fe9766cc7583b0956a4a9689
Skytec711 <asahi79289@gmail.com>
2022-09-14 14:30:26 +0800

    Feat: logger.mjs

---
+++ logger.mjs
@@ -0,0 +1,34 @@
+function getDateTimeNow() {
+    let dateNow = new Date();
+
+    // current date
+    // adjust 0 before single digit date
+    let date = ("0" + dateNow.getDate()).slice(-2);
+
+    // current month
+    let month = ("0" + (dateNow.getMonth() + 1)).slice(-2);
+
+    // current year
+    let year = ("0" + dateNow.getFullYear()).slice(-2);
+
+    // current hours
+    let hours = ("0" + dateNow.getHours()).slice(-2);
+
+    // current minutes
+    let minutes = ("0" + dateNow.getMinutes()).slice(-2);
+
+    // current seconds
+    let seconds = ("0" + dateNow.getSeconds()).slice(-2);
+
+    return `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`
+}
+
+function log(prefix, text, postfix=false) {
+    if (!postfix) {
+        console.log(`[${getDateTimeNow()}] (${prefix}) ${text}`);
+        return
+    }
+    console.log(`[${getDateTimeNow()}] (${prefix}) ${text} @ ${postfix}`);
+}
+
+export { getDateTimeNow, log };
