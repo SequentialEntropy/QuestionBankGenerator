603e8bd1983c87437fa4e32386a9ed1f55cd49ad
Skytec711 <asahi79289@gmail.com>
2022-09-26 22:49:54 +0800

    Refactor: idTracker.mjs uses jsonReader.mjs

--- database/idTracker.mjs
+++ database/idTracker.mjs
@@ -1,12 +1,6 @@
-import { promises as fs } from "fs";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
 import { log } from "../logger.mjs";
 
-const __filename = fileURLToPath(
-    import.meta.url);
-
-const __dirname = dirname(__filename);
+import { jsonReader } from "./jsonReader.mjs";
 
 const defaults = {
     nextId: 0,
@@ -14,92 +8,53 @@ const defaults = {
 }
 
 class Tracker {
-    constructor(data, filePath) {
-        this._filePath = filePath;
-        this._load(data);
-
+    constructor(jsonRW) {
+        this._jsonRW = jsonRW;
+        
         this._log("Initialised");
-    };
+    }
     static async init(filePath) {
-        log("ID Tracker static", "Initialising Tracker");
-        let data = defaults;
-        try {
-            data = await Tracker._readFromFile(filePath);
-        } catch (err) {
-            if (err.code === "ENOENT") { // File not found
-                log("ID Tracker static", "File not found");
-                Tracker._writeToFile(data, filePath);
-                log("ID Tracker static", "Created default file", filePath);
-            } else {
-                throw err;
-            }
-        };
-        return new Tracker(data, filePath);
+        let jsonRW = jsonReader.init(filePath, defaults);
+        return new Tracker(await jsonRW);
+    }
+    _data() {
+        return this._jsonRW.data;
+    }
+    _save() {
+        this._jsonRW.save();
     }
     create() {
-        this._log("Create function called");
+        this._log("create() function called");
 
-        const id = this._nextId;
-        this._nextId++;
-        this._log(`Updated nextId to ${this._nextId}`);
+        const id = this._data().nextId;
+        this._data().nextId++;
+        this._log("Updated nextId", this._data().nextId);
         
-        if (this._list.includes(id)) {
-            this._log(`Detected existing ID ${id}`);
+        if (this._data().list.includes(id)) {
+            this._log("Detected existing ID", id);
             return id;
         }
 
-        this._list.push(id);
-        this._log(`Pushed new ID ${id}`);
+        this._data().list.push(id);
+        this._log("Pushed new ID", id);
 
-        Tracker._writeToFile(this._save(), this._filePath);
+        this._save();
+
         return id;
     }
     remove(id) {
-        this._log("Remove function called");
+        this._log(`remove(${id}) function called`);
 
-        const index = this._list.indexOf(id);
-        this._list.splice(index, 1);
-        this._log(`Spliced index ${index}`);
+        const index = this._data().list.indexOf(id);
+        this._data().list.splice(index, 1);
+        this._log("Spliced index", index);
 
-        Tracker._writeToFile(this._save(), this._filePath);
-        this._log(`Updated file to reflect removed ID ${id}`);
+        Tracker._writeToFile(this._save(), this._data()._filePath);
+        this._log("Updated file to reflect removed ID", id);
     }
-    static async _readFromFile(filePath) {
-        log("ID Tracker static", "Read function called");
-
-        const rawData = await fs.readFile(filePath);
-        log("ID Tracker static", "File read", rawData);
-        const data = JSON.parse(rawData);
-        log("ID Tracker static", "String -> JSON - Parsed read file", data);
-        
-        return data;
-    }
-    async _load(data) {
-        this._log("Load function called");
-
-        this._nextId = data.nextId;
-        this._list = data.list;
-
-        this._log("Load done");
-    }
-    static async _writeToFile(data, filePath) {
-        log("ID Tracker static", "Write function called", filePath);
-
-        const rawData = JSON.stringify(await data, null, 4);
-        log("ID Tracker static", "JSON -> String - Prepared stringified data for writing");
-        fs.writeFile(filePath, rawData);
-        log("ID Tracker static", "File written", filePath);
-    }
-    async _save() {
-        this._log("Save function called");
-        return {
-            nextId: this._nextId,
-            list: this._list
-        };
-    }
-    _log(text) {
-        log(`ID Tracker`, text, this._filePath);
+    _log(text, postfix=false) {
+        log(`ID Tracker ${this._jsonRW.fileName()}`, text, postfix);
     }
 }
 
-export { Tracker };
+export { Tracker }
