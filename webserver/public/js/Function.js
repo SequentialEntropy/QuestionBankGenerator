30d8c9ed4a6c1f8c1366ef010d2efef79ceae648
Skytec711 <asahi79289@gmail.com>
2022-12-20 18:38:50 +0800

    Feat: Function delete button

--- webserver/public/js/Function.js
+++ webserver/public/js/Function.js
@@ -1,29 +1,87 @@
 import { VariableField, OperationField, Prompt } from "./Field.js";
 
-class RenderFunction {
-    builder(className, shelf) {
-        const root = document.createElement("div");
-        root.classList.add("Function");
-        root.classList.add(className);
-        shelf.forEach(e => {
-            root.appendChild(e.root);
-        })
-        return root;
+class Function {
+    buildShelf(className, shelfContent) {
+        const shelf = document.createElement("div");
+        shelf.classList.add("Function-shelf");
+        shelf.classList.add(className);
+        shelfContent.forEach(e => {
+            shelf.appendChild(e.root);
+        });
+        return shelf;
     }
     createRoot() {
-        return this.builder("block__render", [
+        const root = document.createElement("div");
+        root.classList.add("Function");
+
+        return root;
+    }
+    createDeleteButton() {
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("Block-delete");
+        deleteButton.textContent = "×";
+
+        return deleteButton;
+    }
+    createShelf() {
+        return this.shelfBuilder("block__default", []);
+    }
+    constructor() {
+        this.root = this.createRoot();
+        this.shelf = this.createShelf();
+        this.deleteButton = this.createDeleteButton();
+
+        this.root.appendChild(this.shelf);
+        this.root.appendChild(this.deleteButton);
+        /*
+        <div class="Function">
+            <div class="Function-shelf">
+            </div>
+            <button class="Block-delete">
+            ×
+            </button>
+        </div>
+        */
+        this.root.addEventListener("mouseover", e => {
+            e.stopPropagation();
+            this.shelf.classList.add("Function-shelf__hover");
+            this.deleteButton.classList.add("Block-delete__show");
+        });
+        this.root.addEventListener("mouseout", e => {
+            e.stopPropagation();
+            this.shelf.classList.remove("Function-shelf__hover");
+            this.deleteButton.classList.remove("Block-delete__show");
+        })
+
+        this.deleteButton.addEventListener("click", e => {
+            if (!confirm(`Are you sure you want to delete the selected Function?`)) {
+                return;
+            }
+
+            const event = new Event("delete");
+
+            const parentField = this.root.closest(".FunctionsShelf");
+
+            parentField.dispatchEvent(event);
+        })
+    }
+}
+
+class RenderFunction extends Function {
+    createShelf() {
+        return this.buildShelf("block__render", [
             new Prompt("Render"),
             new OperationField()
         ]);
     }
     constructor() {
-        this.root = this.createRoot();
+        super();
     }
 }
 
 class SetFunction extends RenderFunction {
-    createRoot() {
-        return this.builder("block__operation", [
+    createShelf() {
+        return this.buildShelf("block__operation", [
             new Prompt("Set the value of"),
             new VariableField(),
             new Prompt("to"),
