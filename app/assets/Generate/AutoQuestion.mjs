bed42d391d969ed91f7cb7c241256243e990e3e7
Skytec711 <asahi79289@gmail.com>
2023-03-09 19:16:24 +0800

    Feat: rollVariables

--- app/assets/Generate/AutoQuestion.mjs
+++ app/assets/Generate/AutoQuestion.mjs
@@ -1,3 +1,5 @@
+import Decimal from "../../libs/decimal.mjs";
+
 export default class AutoQuestion {
     createRoot() {
         return document.createElement("div");
@@ -9,5 +11,21 @@ export default class AutoQuestion {
         this.variableOptions = variableOptions;
 
         this.root.textContent = this.questionNumber;
+
+        this.variables = {};
+
+        this.rollVariables();
+    }
+    rollVariables() {
+        this.variableOptions.forEach(variable => {
+            this.variables[variable.variableName] = this.roll(variable.minimum, variable.maximum, variable.order).toString();
+        });
+    }
+    roll(min, max, order) {
+        const result = Decimal.random().times(max.minus(min).plus(min));
+
+        const rounded = ((result.dividedBy(order)).round(order)).times(order);
+
+        return rounded
     }
 }
