8626b082d596d2f090a982f67f9b3f04eb4ad99c
Skytec711 <asahi79289@gmail.com>
2023-03-09 20:01:51 +0800

    Feat: root structure for AutoQuestion

--- app/assets/Generate/AutoQuestion.mjs
+++ app/assets/Generate/AutoQuestion.mjs
@@ -2,22 +2,30 @@ import Decimal from "../../libs/decimal.mjs";
 
 export default class AutoQuestion {
     createRoot() {
-        return document.createElement("div");
+        const range = document.createRange();
+
+        range.selectNode(document.body);
+
+        return range.createContextualFragment(`
+        <div class="section theme__background--drop-down">
+            <div class="section-drop-zone"></div>
+            <div class="section__area theme__color--default">
+                <div class="section__heading">
+                    <div class="section__title">Question #</div>
+                </div>
+            </div>
+        </div>
+        `).children[0];
     }
     constructor(questionNumber, variableOptions) {
         this.root = this.createRoot();
-
-        this.questionNumber = questionNumber;
-        this.variableOptions = variableOptions;
-
-        this.root.textContent = this.questionNumber;
+        this.root.querySelector(".section__title").textContent = `Question ${questionNumber}`;
 
         this.variables = {};
-
-        this.rollVariables();
+        this.rollVariables(variableOptions);
     }
-    rollVariables() {
-        this.variableOptions.forEach(variable => {
+    rollVariables(variableOptions) {
+        variableOptions.forEach(variable => {
             this.variables[variable.variableName] = this.roll(variable.minimum, variable.maximum, variable.order).toString();
         });
     }
